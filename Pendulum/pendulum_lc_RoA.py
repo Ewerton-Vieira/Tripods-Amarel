1cb3050f9bc484bf51c787f37187ac9d6a2fcb9c
Ewerton Rocha Vieira <ewerton@ofrkbrcsl06.rad.rutgers.edu>
2022-01-04 00:16:46 -0500

    a

--- Pendulum/pendulum_lc_RoA.py
+++ Pendulum/pendulum_lc_RoA.py
@@ -17,8 +17,8 @@ from datetime import datetime
 MG_util = CMGDB_util.CMGDB_util()
 
 
-sb = 14
-time = 30  # time is equal to 10s
+sb = 12
+time = 5  # time is equal to 10s
 
 TM = TimeMap.TimeMap("pendulum_lc", time,
                      "examples/tripods/lc_roa.yaml")
@@ -35,12 +35,6 @@ y_min = -6.28318
 y_max = 6.28318
 
 
-# x_min = -1
-# x_max = 1
-# y_min = -1
-# y_max = 1
-
-
 # base name for the output files.
 base_name = "learned_pendulum_time" + \
     str(time) + "_" + \
@@ -52,22 +46,45 @@ print(base_name)
 
 # ### Loading functions
 
-
-def g(X):
-    return TM.pendulum_lc(X)
-
-
-print(g([0.6, 0.6]))
+#
+# def g(X):
+#     return TM.pendulum_lc(X)
+#
+#
+# print(g([0.6, 0.6]))
 
 
 # Define the parameters for CMGDB
 lower_bounds = [x_min, y_min]
 upper_bounds = [x_max, y_max]
 
+
+# load map
+
+startTime = datetime.now()
+
+file_name = base_name + ".csv"
+map = grid.load_map_grid(file_name, lower_bounds, upper_bounds, sb)
+
+print(f"Time to load map = {datetime.now() - startTime}")
+
+
+def g_on_grid(x):
+    return grid.image_of_vertex_from_loaded_map(map, x, lower_bounds, upper_bounds, sb)
+
+
+# test
+vertex = [(upper_bounds[a] - lower_bounds[a])/(2**(1 + (a & 1))) for a in range(len(upper_bounds))]
+
+print("region, vertex coordinates", grid.vertex2grid_vertex(
+    vertex, lower_bounds, upper_bounds, sb))
+print(g_on_grid(vertex))
+
+
 phase_periodic = [True, False]
 
-# K = sampled_Lipschitz(lower_bounds, upper_bounds, N, g, base_name)
-K = [1.1, 1.1]
+
+K = 1.05
 
 
 # def F(rect):
@@ -91,17 +108,17 @@ K = [1.1, 1.1]
 
 # fig, ax = ROA.PlotMorseTiles(lower_bounds, upper_bounds, from_file=base_name)
 
-fig, ax = ROA.PlotMorseTiles(lower_bounds, upper_bounds,
-                             from_file=base_name, from_file_basic=True)
+# fig, ax = ROA.PlotMorseTiles(lower_bounds, upper_bounds,
+#                              from_file=base_name, from_file_basic=True)
 
 
-# dynamics
-
-TM1 = TimeMap.TimeMap("pendulum_lc", 0.01,
-                      "examples/tripods/lc_roa.yaml")
-
-fig, ax = dyn_tools.Plot_trajectories(lower_bounds, upper_bounds, TM1.pendulum_lc, fig=fig, ax=ax, xlim=[
-                                      lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]])
-
-plt.savefig(base_name)
-plt.show()
+# # dynamics
+#
+# TM1 = TimeMap.TimeMap("pendulum_lc", 0.01,
+#                       "examples/tripods/lc_roa.yaml")
+#
+# fig, ax = dyn_tools.Plot_trajectories(lower_bounds, upper_bounds, TM1.pendulum_lc, fig=fig, ax=ax, xlim=[
+#                                       lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]])
+#
+# plt.savefig(base_name)
+# plt.show()
