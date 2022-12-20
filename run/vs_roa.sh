2ca7d9b6b11beea1c85eeaff9b3eb34eb46961f8
ewerton-vieira <ewertonrvieira@gmail.com>
2022-12-19 21:37:54 -0500

    a

--- run/vs_roa.sh
+++ run/vs_roa.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1                    # Total number of tasks across all nodes
 #SBATCH --cpus-per-task=1             # Number of CPUs (cores) per task (>1 if multithread tasks)
 #SBATCH --mem=32000                    # Real memory (RAM) required (MB)
-#SBATCH --time=24:00:00               # Total run time limit (hh:mm:ss)
+#SBATCH --time=8:00:00               # Total run time limit (hh:mm:ss)
 #SBATCH --requeue                     # Return job to the queue if preempted
 #SBATCH --export=ALL                  # Export you current env to the job env
 
@@ -19,5 +19,5 @@
 cd /scratch/er691/Tripods-Amarel/Visual_S/
 
 #  Run python script with input data
-srun python Visual_S_data_extract.py
+# srun python Visual_S_data_extract.py
 srun python visual_S_RoA.py
