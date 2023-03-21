06cd026069c84460c96e138674d943c4c321f788
ewerton-vieira <ewertonrvieira@gmail.com>
2023-03-21 01:04:04 -0400

    .

--- run/run_all_aemg.sh
+++ run/run_all_aemg.sh
@@ -1,8 +1,9 @@
 cd /scratch/er691/TRIPODS-AMAREL/AEMG/run
 
+dir_files=/scratch/er691/TRIPODS-AMAREL/AEMG/run
 # cd ~/TRIPODS-AMAREL/AEMG/run
 
-yourfilenames='ls $(pwd)/*.sh'
+yourfilenames='ls /scratch/er691/TRIPODS-AMAREL/AEMG/run/*.sh'
 
 for eachfile in $yourfilenames
 do
