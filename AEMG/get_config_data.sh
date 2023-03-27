926debebd756dadf1b0069aed17c7d8a474e5518
ewerton-vieira <ewertonrvieira@gmail.com>
2023-03-27 12:28:18 -0400

    .

--- AEMG/get_config_data.sh
+++ AEMG/get_config_data.sh
@@ -4,19 +4,23 @@ system='ndpendulum'
 underline='_'
 control='lqr'
 
-cd $dir_path
-python generate_config.py --config $system.txt
-
-
 k="000"
 w="k"
 
 
-for i in 1 10 100
-do
-    python get_data.py --num_trajs "$i$k" --save_dir "/data/$system$underline$control$i$w/" --system 'ndpendulum'
-done
+# cd $dir_path
+# python generate_config.py --config $system.txt
+
+# for i in 1 10 100
+# do
+#     python get_data.py --num_trajs "$i$k" --save_dir "/data/$system$underline$control$i$w/" --system 'ndpendulum'
+# done
 
 cd '/scratch/er691/Tripods-Amarel/AEMG'
-python run.py --system ndpendulum --control lqr
+
+for i in 1 10 100
+do
+    python run.py --system $system --control $control --dir_run "/run/$system$underline$control$i$w"
+done
+
 
